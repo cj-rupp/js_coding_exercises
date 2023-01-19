ef34d16b820b8d503d6bbb0e82c5d09c9326c3eb
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-01-19 17:29:12 +0000

    Implemented 7 out of 10 exercise functions

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -2,40 +2,66 @@
 
 export function capitalize(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word[0].toUpperCase() + word.substring(1); 
 }
 
 export function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
-  // Add your code here!
+  return firstName[0] + "." + lastName[0];
 }
 
 export function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-  // Add your code here!
+  const newPrice = originalPrice * (1 + (vatRate/100));
+  if(Number.isInteger(newPrice)){
+    return newPrice;
+  }
+  else{
+    return parseFloat(newPrice.toFixed(2));
+  }
 }
 
 export function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  const newPrice = originalPrice * (1 - (reduction/100));
+  if(Number.isInteger(newPrice)){
+    return newPrice;
+  }
+  else{
+    return parseFloat(newPrice.toFixed(2));
+  }
 }
 
 export function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  const strLen = str.length;
+  const roughMiddle = strLen/2;
+  console.warn(roughMiddle);
+  if((strLen % 2) === 0) {
+    return str[roughMiddle-1] + str[roughMiddle];
+  }
+  else {
+    const result = str[Math.trunc(roughMiddle)];
+    return result;
+  }
 }
 
 export function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  const letters = word.split("");
+  let rWord = "";
+  letters.forEach(letter => {
+    rWord = letter + rWord;
+  });
+  return rWord;
 }
 
 export function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  return words.map(word => reverseWord(word));
 }
 
 export function countLinuxUsers(users) {
