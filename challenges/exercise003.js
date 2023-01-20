d00871e8b7c5bdc911e2818fb2834cb6ea2b7ef4
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-01-20 20:32:59 +0000

    Implemented 3 of 5 functions

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,11 +1,13 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  return nums.map((num) => (num**2));
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  return words.map((word,pos) => (
+    (pos === 0) ? word : word[0].toUpperCase() + word.substring(1)
+  )).join("");
 }
 
 export function getTotalSubjects(people) {
